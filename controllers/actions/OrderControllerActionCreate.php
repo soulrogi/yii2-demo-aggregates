b3492ba6be11df9c35ea9cfa492e97f6a2fd4c7f
Igor <soulrogi@gmail.com>
2020-06-04 06:47:38 +1000

    Доменные сущности и ActiveRecord -

--- controllers/actions/OrderControllerActionCreate.php
+++ controllers/actions/OrderControllerActionCreate.php
@@ -6,10 +6,6 @@ namespace app\controllers\actions;
 
 use app\controllers\OrderController;
 use app\domain\entities\Order\Decorators\Helpers\DecoratorHelper;
-use app\domain\entities\Order\Decorators\Implementations\Delivery\DeliveryDecoratorInterface;
-use app\domain\entities\Order\Decorators\Implementations\Delivery\ValueObjects\Delivery;
-use app\domain\entities\Order\Decorators\Implementations\Discount\DiscountDecoratorInterface;
-use app\domain\entities\Order\Decorators\Implementations\Discount\ValueObjects\Discount;
 use app\domain\services\Order\OrderService;
 use app\domain\yii2\forms\OrderCreateForm;
 use DomainException;
@@ -39,21 +35,16 @@ class OrderControllerActionCreate extends Action {
 
 	public function run(): Response {
 		try {
-			$order = $this->service->create();
-			$items = $order->getGoods()->remove('e691b6ee-fe47-42d7-81c7-0794593f29f6');
-
-			$discountNames = array_map(function(Discount $discount): string {
-				return $discount->getName();
-			}, DecoratorHelper::getPayloadByInterface($order, DiscountDecoratorInterface::class));
-
-			/** @var Delivery[] $deliveryId */
-			$deliveryId = DecoratorHelper::getPayloadByInterface($order, DeliveryDecoratorInterface::class);
+			$order      = $this->service->create();
+			$items      = $order->getGoods()->remove('e691b6ee-fe47-42d7-81c7-0794593f29f6');
+			$discounts  = DecoratorHelper::getDiscounts($order);
+			$deliveries = DecoratorHelper::getDeliveries($order);
 
 			return $this->controller->asJson([
 				'OrderId'        => $order->getId()->getId(),
 				'Removed Item'   => $items->getId(),
-				'DiscountsNames' => $discountNames,
-				'DeliveryId'     => end($deliveryId)->getId(),
+				'DiscountsNames' => $discounts,
+				'DeliveryId'     => end($deliveries)->getId(),
 			]);
 		}
 		catch (DomainException $e) {
--- domain/entities/Order/Decorators/Helpers/DecoratorHelper.php
+++ domain/entities/Order/Decorators/Helpers/DecoratorHelper.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace app\domain\entities\Order\Decorators\Helpers;
 
 use app\domain\entities\Order\Decorators\DecoratorInterface;
+use app\domain\entities\Order\Decorators\Implementations\Delivery\DeliveryDecoratorInterface;
+use app\domain\entities\Order\Decorators\Implementations\Delivery\ValueObjects\Delivery;
+use app\domain\entities\Order\Decorators\Implementations\Discount\DiscountDecoratorInterface;
+use app\domain\entities\Order\Decorators\Implementations\Discount\ValueObjects\Discount;
 use app\domain\entities\Order\Decorators\Strategies\Unpacker\UnpackerStrategyProvider;
 use app\domain\entities\Order\OrderInterface;
 
@@ -15,7 +19,7 @@ class DecoratorHelper {
 	 *
 	 * @return array
 	 */
-	public static function getPayloadByInterface(OrderInterface $order, string $interface): array {
+	static public function getPayloadByInterface(OrderInterface $order, string $interface): array {
 		if (false === ($order instanceof DecoratorInterface)) {
 			return [];
 		}
@@ -28,5 +32,20 @@ class DecoratorHelper {
 		$subResult = static::getPayloadByInterface($order->getOrder(), $interface);
 
 		return array_merge($result, $subResult);
+	}
+
+	static public function getDiscounts(OrderInterface$order):array {
+		return array_map(function(Discount $discount): string {
+			return $discount->getName();
+		}, static::getPayloadByInterface($order, DiscountDecoratorInterface::class));
+	}
+
+	/**
+	 * @param OrderInterface $order
+	 *
+	 * @return Delivery[]
+	 */
+	static public function getDeliveries(OrderInterface$order):array {
+		return static::getPayloadByInterface($order, DeliveryDecoratorInterface::class);
 	}
 }
